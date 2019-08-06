dc6defb3a0bdc8a74138b09e64451918a1fe3703
seko17 <34337236+seko17@users.noreply.github.com>
2019-08-06 13:00:48 +0200

    commit

--- src/pages/landhome/landhome.ts
+++ src/pages/landhome/landhome.ts
@@ -6,7 +6,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProvidersUserProvider } from '../../providers/providers-user/providers-user';
 import { HomePage } from '../home/home';
 import { LoginmodalPage } from '../loginmodal/loginmodal';
-
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 /**
  * Generated class for the LandhomePage page.
@@ -21,8 +22,12 @@ import { LoginmodalPage } from '../loginmodal/loginmodal';
   templateUrl: 'landhome.html',
 })
 export class LandhomePage {
-
+user
   constructor(public navCtrl: NavController, public navParams: NavParams, private authService : ProvidersUserProvider) {
+  this.user = firebase.auth().currentUser.uid
+  console.log(this.user)
+  this.authService.setUser(this.user);
+
   }
 
   ionViewDidLoad() {
